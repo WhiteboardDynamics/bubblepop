ffc40243095537c1fbb010aa32650704f81eb6b0
torchhound <unknownmasayoshi@gmail.com>
2018-08-03 03:24:20 -0400

    Added lose condition, expansion and contraction, Finished unless additional features are required

--- bubblepop.js
+++ bubblepop.js
@@ -2,13 +2,14 @@ const canvas = document.getElementById('game');
 const context = canvas.getContext("2d");
 const bubblePoints = 50;
 const startTime = Math.round((new Date()).getTime() / 1000);
-var spawnRate = 1; //increase or decrease as time goes on?
+const expansionContraction = .20;
+var spawnRate = 1;
 var score = 0;
 var bubbles = [];
 var spawnNumber = 1;
 
-function isIntersect(point, circle) {
-  return Math.sqrt((point.x - circle.x) ** 2 + (point.y - circle.y) ** 2) < circle.r;
+function doesIntersect(point, circle) {
+  return Math.sqrt((point.x - circle.x) ** 2 + (point.y - circle.y) ** 2) < circle.radius;
 }
 
 function clickHandler(event) {
@@ -16,44 +17,63 @@ function clickHandler(event) {
     x: event.clientX,
     y: event.clientY
   };
-  for (var i = bubbles.length - 1; i >= 0; i--) {
-    if (isIntersect(position, bubbles[i])) {
+
+  bubbles.forEach(function(bubble, index, bubbles) {
+    if (doesIntersect(position, bubbles[index])) {
       score += 10;
-      bubbles.splice(i, 1);
+      bubbles.splice(index, 1);
     }
-  }
+  });
 }
 
 function drawScore() {
-  context.font = '25px Helvetica'; //scaling?
+  context.font = '3vw Helvetica';
+  context.fillStyle = '#000000';
   context.fillText(`Score: ${score}`, 10, 30);
 }
 
-function drawCircle(x, y, r) {
+function drawCircle(x, y, radius, color) {
+  if (radius <= 0) {
+    return false;
+  }
   context.beginPath();
-  context.arc(x, y, r, 0, Math.PI * 2);
-  context.fillStyle = '#0000ff'; //random color?
+  context.arc(x, y, radius, 0, Math.PI * 2);
+  context.fillStyle = color;
   context.fill();
   context.closePath();
 }
 
 function spawnBubbles() {
   if (Math.round((new Date()).getTime() / 1000) > startTime + spawnRate) {
-    spawnNumber++;
-    spawnRate++;
+    spawnNumber += .35;
+    spawnRate += 2;
   }
 
   if (bubbles.length < spawnNumber) {
     for (var i = spawnNumber; i >= 0; i--) {
-      const x = Math.random() * (canvas.width - 10) + 10;
-      const y = Math.random() * (canvas.height - 30) + 30;
-      const r = Math.random() * (50 - 5) + 5;
-      bubbles.push({x: x, y: y, r: r});
+      const x = Math.floor(Math.random() * (canvas.width - 10 +1 )) + 10;
+      const y = Math.floor(Math.random() * (canvas.height - 30 + 1)) + 30;
+      const r = Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+      const c = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+      bubbles.push({x: x, y: y, radius: r, ascending: true, color: c});
     }
   }
 
-  bubbles.forEach(function(bubble) {
-    drawCircle(bubble.x, bubble.y, bubble.r);
+  bubbles.forEach(function(bubble, index, bubbles) {
+    const negativeCheck = drawCircle(bubble.x, bubble.y, bubble.radius, bubble.color);
+
+    if (negativeCheck === false) {
+      score -= 10;
+      bubbles.splice(index, 1);
+    } else {
+      if (bubble.radius < 50 && bubble.ascending === true) {
+        bubbles[index] = {x: bubble.x, y: bubble.y, 
+          radius: bubble.radius + expansionContraction, ascending: true, color: bubble.color};
+      } else {
+        bubbles[index] = {x: bubble.x, y: bubble.y, 
+          radius: bubble.radius - expansionContraction, ascending: false, color: bubble.color}; 
+      }
+    }
   });
 }
 
@@ -64,7 +84,13 @@ function draw() {
   drawScore();
   spawnBubbles();
 
-  requestAnimationFrame(draw);
+  if (score >= 0) {
+    requestAnimationFrame(draw);
+  } else {
+    context.font = '7vw Helvetica';
+    context.fillStyle = '#000000';
+    context.fillText('Game Over...', canvas.width / 2 - 175, canvas.height / 2);
+  }
 }
 
 window.addEventListener('click', clickHandler, false);

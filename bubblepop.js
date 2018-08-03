3ff0175b66f59364a276da11f92b32816c7b5020
torchhound <unknownmasayoshi@gmail.com>
2018-08-03 17:37:56 -0400

    Added movement vectors and bouncing

--- bubblepop.js
+++ bubblepop.js
@@ -3,6 +3,7 @@ const context = canvas.getContext("2d");
 const bubblePoints = 50;
 const startTime = Math.round((new Date()).getTime() / 1000);
 const expansionContraction = .20;
+const bubbleVelocity = .25;
 let spawnRate = 1;
 let score = 0;
 let bubbles = [];
@@ -60,6 +61,68 @@ function randomColor() {
   return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 }
 
+// Returns a random direction
+function randomDirection() {
+  const seed = randomInteger(0, 7);
+  switch (seed) {
+    case 0:
+      return 'n';
+    case 1:
+      return 's';
+    case 2:
+      return 'e';
+    case 3:
+      return 'w';
+    case 4:
+      return 'ne';
+    case 5:
+      return 'nw';
+    case 6:
+      return 'se';
+    case 7:
+      return 'sw';
+    default:
+      return 'n';
+  }
+}
+
+// Returns the opposite of the given direction
+function reverseDirection(direction) {
+  switch (direction) {
+    case 'n':
+      return 's';
+    case 's':
+      return 'n';
+    case 'e':
+      return 'w';
+    case 'w':
+      return 'e';
+    case 'ne':
+      return 'se';
+    case 'nw':
+      return 'sw';
+    case 'se':
+      return 'ne';
+    case 'sw':
+      return 'nw';
+    default:
+      return direction
+  }
+}
+
+// Checks if a bubble is at the edge and reverses its direction if it is
+function edgeCheck(x, y, radius, direction) {
+  if (x + bubbleVelocity >= canvas.width - radius 
+    || x + bubbleVelocity <= radius 
+    || y + bubbleVelocity >= canvas.height - radius
+    || y + bubbleVelocity <= radius) {
+    return reverseDirection(direction);
+  } else {
+    return direction;
+  }
+}
+
+// Creates more bubbles if necessary and modifies their position
 function spawnBubbles() {
   if (Math.round((new Date()).getTime() / 1000) > startTime + spawnRate) {
     spawnNumber += .35;
@@ -72,11 +135,13 @@ function spawnBubbles() {
       const x = randomInteger(10, canvas.width);
       const y = randomInteger(30, canvas.height);
       const c = randomColor();
+      const d = randomDirection();
       bubbles.push({x: x,
                     y: y,
                     radius: radius,
                     ascending: true,
-                    color: c});
+                    color: c,
+                    direction: d});
     }
   }
 
@@ -87,35 +152,62 @@ function spawnBubbles() {
       score -= 10;
       bubbles.splice(index, 1);
     } else {
-      const coinFlip = randomInteger(0, 1);
       let bubbleX = bubble.x;
       let bubbleY = bubble.y;
 
-      if (coinFlip === 0) {
-        bubbleX++;
-        bubbleY++;
-      } else {
-        bubbleX--;
-        bubbleY--;
+      const direction = edgeCheck(bubble.x, bubble.y, bubble.radius, bubble.direction);
+
+      switch (direction) {
+        case 'n':
+          bubbleY += bubbleVelocity;
+          break;
+        case 's':
+          bubbleY -= bubbleVelocity;
+          break;
+        case 'e':
+          bubbleX += bubbleVelocity;
+          break;
+        case 'w':
+          bubbleX -= bubbleVelocity;
+          break;
+        case 'ne':
+          bubbleX += bubbleVelocity;
+          bubbleY += bubbleVelocity;
+          break;
+        case 'nw':
+          bubbleX -= bubbleVelocity;
+          bubbleY += bubbleVelocity;
+          break;
+        case 'se':
+          bubbleX += bubbleVelocity;
+          bubbleY -= bubbleVelocity;
+          break;
+        case 'sw':
+          bubbleX -= bubbleVelocity;
+          bubbleY -= bubbleVelocity;
+          break;
       }
-      
+
       if (bubble.radius < 50 && bubble.ascending === true) {
         bubbles[index] = {x: bubbleX,
                           y: bubbleY,
                           radius: bubble.radius + expansionContraction,
                           ascending: true,
-                          color: bubble.color};
+                          color: bubble.color,
+                          direction: direction};
       } else {
         bubbles[index] = {x: bubbleX,
                           y: bubbleY,
                           radius: bubble.radius - expansionContraction,
                           ascending: false,
-                          color: bubble.color};
+                          color: bubble.color,
+                          direction: direction};
       }
     }
   });
 }
 
+// Redraws the screen each tick
 function draw() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
